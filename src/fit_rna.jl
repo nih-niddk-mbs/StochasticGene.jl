1b8680153046165dde901c454d1f896498871b17
Carson Chow <ccc1685@gmail.com>
2020-12-17 20:06:22 -0500

    update

--- src/fit_rna.jl
+++ src/fit_rna.jl
@@ -11,12 +11,17 @@ function fit_rna()
     # Paths
     root = "/home/carsonc/scrna/"
     # root = "/Users/carsonc/Box/scrna"
-    resultfolder = "2020-12-17"
+    resultfolder = "Results/2020-12-17"
     datafolder = "smRNA_FISH_HCT_7timepoints"
-    ratefolder = "2020-12-16/rates_IAA_FISH_Transient_Delay_Myc_3_2.txt"
-    ratefolder = joinpath(root,"Results/" * ratefolder)
-    path = joinpath(root,"data/" * datafolder)
-    datafiles = [joinpath(path,"T0_IAA");joinpath(path,"T30_IAA");joinpath(path,"T120_IAA")]
+    datapath = joinpath(root,"data/" * datafolder)
+    datafiles = [joinpath(datapath,"T0_IAA");joinpath(datapath,"T30_IAA");joinpath(datapath,"T120_IAA")]
+
+    infolder = "Results/2020-12-17"
+    ratefile = "rates_IAA_FISH_Transient_Delay_Myc_3_2.txt"
+    paramfile = "param_stats_IAA_FISH_Transient_Delay_Myc_3_2.txt"
+
+    ratefile = joinpath(root, joinpath(infolder,ratefile))
+    paramfile = joinpath(root, joinpath(infolder,paramfile))
 
     # Data parameters
     gene = "Myc"
@@ -24,19 +29,22 @@ function fit_rna()
     times = [0.;30.;120.]
 
     #Model parameters
-    rMYC = StochasticGene.read_rates(ratefolder,1)
+    rMYC = StochasticGene.read_rates(ratefile,1)
     decayprior = log(2)/(halflife*60)
     delayprior = .05
     G = 3
     nalleles = 2
-    cv = .01
+    cv = StochasticGene.read_covlogparam(paramfile)
+    if ~isposdef(cv)
+        cv = .01
+    end
     fitted = [7;8;9;10;11;13]
 
     # Metropolis-Hastings parameters
-    maxtime = 3600*4.
+    maxtime = 3600*12.
     samplesteps = 10000
-    warmupsteps = 1000
-    temp = 2.
+    warmupsteps = 0
+    temp = 10.
     method = 1
 
     println(gene," ",G)
@@ -45,7 +53,7 @@ function fit_rna()
     nchains = nworkers()
     @time fit,stats,waic = StochasticGene.run_mh(data,model,options,nchains);
 
-    writefile = joinpath(root,"Results/" * resultfolder)
+    writefile = joinpath(root,resultfolder)
     StochasticGene.write_all(writefile,fit,stats,waic,data,model)
 
     println(fit.accept," ",fit.total)
