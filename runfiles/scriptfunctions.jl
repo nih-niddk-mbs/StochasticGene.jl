f8b250c38d4880f01d53e46f9ec91d743e9ff99f
Carson C Chow <ccc1685@users.noreply.github.com>
2021-10-07 15:15:01 -0400

    typo fixed

--- runfiles/scriptfunctions.jl
+++ runfiles/scriptfunctions.jl
@@ -44,8 +44,7 @@ end
 
 function fit_rna(nchains::Int,gene::String,fittedparam::String,fixedeffects::String,datacond,G::Int,maxtime::Float64,infolder::String,resultfolder::String,datafolder,inlabel,label,nsets,runcycle::Bool=false,transient::Bool=false,samplesteps::Int=100000,warmupsteps=20000,annealsteps=0,temp=1.,tempanneal=100.,root = "/home/carsonc/scrna/")
     fittedparam = parse.(Int,split(fittedparam,"-"))
-
-    fixedeffects = make_fixedeffects(fixedeffcts)
+    fixedeffects = make_fixedeffects(fixedeffects)
     fit_rna(nchains,gene,fittedparam,fixedeffects,datacond,G,maxtime,infolder,resultfolder,datafolder,inlabel,label,nsets,runcycle,transient,samplesteps,warmupsteps,annealsteps,temp,tempanneal,root)
     nothing
 end
