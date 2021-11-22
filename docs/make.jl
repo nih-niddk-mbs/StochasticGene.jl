63f679a9be8ed7a4660ea4203f2b33cbeecb1118
Carson C Chow <ccc1685@users.noreply.github.com>
2021-11-22 12:49:20 -0500

    add docs folder

---
+++ docs/make.jl
@@ -0,0 +1,12 @@
+push!(LOAD_PATH,"../src/")
+using StochasticGene
+using Documenter
+makedocs(
+         sitename = "StochasticGene.jl",
+         modules  = [StochasticGene],
+         pages=[
+                "Home" => "index.md"
+               ])
+deploydocs(;
+    repo="github.com/nih-niddk-mbs/StochasticGene.jl",
+)
