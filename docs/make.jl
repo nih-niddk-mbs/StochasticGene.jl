01d87c8fca7c6e927519292cd2bf57df59264342
Carson C Chow <ccc1685@users.noreply.github.com>
2021-11-22 13:08:52 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,5 +8,5 @@ makedocs(
                 "Home" => "index.md"
                ])
 deploydocs(;
-    repo="github.com/nih-niddk-mbs/StochasticGene.jl",
+    repo="github.com/nih-niddk-mbs/StochasticGene",
 )
