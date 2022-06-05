419ab7d97ae0ca819734e2db2602ea045b25ee95
Carson C Chow <ccc1685@users.noreply.github.com>
2022-06-05 14:44:48 -0400

    update test function to reflect changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,7 @@ function fit_rna_test(root=".")
     decayrate = 0.01006327034802035
     model = StochasticGene.model_rna(r,2,nalleles,1,.01,[1,2,3],(),decayrate,.05)
     data = StochasticGene.data_rna(gene,"MOCK","data/HCT116_testdata",fish,"scRNA_test",root)
-    options = StochasticGene.MHOptions(10000,2000,0,0,120.,1.,100.)
+    options = StochasticGene.MHOptions(100000,0,0,0,120.,1.,100.)
     fit,stats,measures = StochasticGene.run_mh(data,model,options,1);
     return stats.meanparam, fit.llml, model
 end
@@ -25,10 +25,9 @@ end
 @testset "StochasticGene" begin
 
         p,ll,model =  fit_rna_test()
-        @test isapprox(p,[0.0097; 0.10; 0.99],rtol=0.05)
-        @test isapprox(ll,4450,rtol=0.05)
+        @test isapprox(ll,1766,rtol=0.05)
 
-        r = [0.01, 0.1, 1.0, 0.01]
+        r = [0.0014, 0.005, 0.0016, 0.01]
         h1,h2 = teststeadystatemodel(r,2,60,2)
 
         @test isapprox(h1,h2,rtol=0.05)
