7762bf7b1ced2d3efceb28de1f26575df91c7428
Carson C Chow <ccc1685@users.noreply.github.com>
2022-08-25 09:01:27 -0400

    test updated

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,22 +2,32 @@ using StochasticGene
 using Test
 
 function fit_rna_test(root=".")
+    G = 2
     gene = "CENPL"
     cell = "HCT116"
     fish = false
     nalleles = 2
+    nsets = 1
+    propcv = 0.05
+    fittedparam = [1,2,3]
+    fixedeffects = ()
+    transitions = ([1,2],[2,1])
+    ejectprior = 0.05
     r = [0.01, 0.1, 1.0, 0.01006327034802035]
     decayrate = 0.01006327034802035
-    model = StochasticGene.model_rna(r,2,nalleles,1,.01,[1,2,3],(),decayrate,.05)
-    data = StochasticGene.data_rna(gene,"MOCK","data/HCT116_testdata",fish,"scRNA_test",root)
-    options = StochasticGene.MHOptions(100000,0,0,0,120.,1.,100.)
+    datacond = "MOCK"
+    datafolder = "data/HCT116_testdata"
+    label = "scRNA_test"
+    data = StochasticGene.data_rna(gene,datacond,datafolder,fish,label)
+    model = StochasticGene.model_rna(data,r,G,nalleles,nsets,propcv,fittedparam,fixedeffects,transitions,decayrate,ejectprior)
+    options = StochasticGene.MHOptions(100000,0,0,30.,1.,100.)
     fit,stats,measures = StochasticGene.run_mh(data,model,options,1);
     return stats.meanparam, fit.llml, model
 end
 
-function teststeadystatemodel(r,G,nhist,nalleles)
-    g1 = StochasticGene.steady_state(r[1:2*G],G-1,nhist,nalleles)
-    g2 = StochasticGene.simulatorGM(r[1:2*G],G-1,nhist,nalleles)
+function teststeadystatemodel(r,transitions,G,nhist,nalleles)
+    g1 = StochasticGene.steady_state(r,transitions,G,nhist,nalleles)
+    g2 = StochasticGene.simulator(r,transitions,G::Int,0,0,nhist,nalleles,collect(1:10))
     return g1, g2
 end
 
@@ -28,7 +38,8 @@ end
         @test isapprox(ll,1766,rtol=0.05)
 
         r = [0.0014, 0.005, 0.0016, 0.01]
-        h1,h2 = teststeadystatemodel(r,2,60,2)
+        transitions = ([1,2],[2,1])
+        h1,h2 = teststeadystatemodel(r,transitions,2,60,2)
 
         @test isapprox(h1,h2,rtol=0.05)
 
