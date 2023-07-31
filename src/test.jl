e6251c4ffdac15d6df61166ddb326460f81c3f42
Carson Chow <ccc1685@gmail.com>
2023-07-31 18:43:52 -0400

    updates

--- src/test.jl
+++ src/test.jl
@@ -73,7 +73,7 @@ options = test_options(1000);
 
 """
 
-function make_trace_vector(r, par, transitions, G, R, onstates, interval, steps, ntrials)
+function simulate_trace_vector(r, par, transitions, G, R, onstates, interval, steps, ntrials)
     trace = Array{Array{Float64}}(undef, ntrials)
     for i in eachindex(trace)
         trace[i] = simulator(r, transitions, G, R, 0, 1, 1, onstates=onstates, traceinterval=interval, totalsteps=steps, par=par)[1:end-1, 2]
@@ -83,11 +83,11 @@ end
 
 simulate_trace(r,transitions,G,R,interval,totaltime,onstates=[G]) = simulator(r, transitions, G, R, 0, 1, 1, onstates=onstates, traceinterval=interval, totaltime=totaltime, par=r[end-3:end])[1:end-1, :]
 
-function test_data(trace, interval)
+function trace_data(trace, interval)
     TraceData("trace", "test", interval, trace)
 end
 
-function test_model(r::Vector, par::Vector, transitions::Tuple, G, R; onstates=[G], propcv=0.05, f=Normal, cv=1.0)
+function trace_model(r::Vector, par::Vector, transitions::Tuple, G, R; onstates=[G], propcv=0.05, f=Normal, cv=1.0)
     ntransitions = length(transitions)
 	npars = length(par)
 	fittedparam = [1:ntransitions+R+1; ntransitions+R+3:ntransitions+R+2+npars]
@@ -95,8 +95,8 @@ function test_model(r::Vector, par::Vector, transitions::Tuple, G, R; onstates=[
 	test_model(r, transitions, G, R, fittedparam, onstates=onstates, propcv=propcv, f=f,cv=cv)
  end
 
-function test_model(r::Vector, transitions::Tuple, G, R, fittedparam; onstates=[G], propcv=0.05, f=Normal, cv=1.)
-	d = test_prior(r, fittedparam,f,cv)
+function trace_model(r::Vector, transitions::Tuple, G, R, fittedparam; onstates=[G], propcv=0.05, f=Normal, cv=1.)
+	d = trace_prior(r, fittedparam,f,cv)
 	method = 1
 	if R == 0
 		components = make_components_T(transitions, G)
@@ -107,18 +107,18 @@ function test_model(r::Vector, transitions::Tuple, G, R, fittedparam; onstates=[
 	end
 end
 
-function test_options(samplesteps::Int=100000, warmupsteps=0, annealsteps=0, maxtime=1000.0, temp=1.0, tempanneal=100.0)
+function trace_options(samplesteps::Int=100000, warmupsteps=0, annealsteps=0, maxtime=1000.0, temp=1.0, tempanneal=100.0)
 
     MHOptions(samplesteps, warmupsteps, annealsteps, maxtime, temp, tempanneal)
 
 end
 
 """
-    test_prior(r,fittedparam,f=Normal)
+    trace_prior(r,fittedparam,f=Normal)
 
 TBW
 """
-function test_prior(r,fittedparam,f=Normal,cv = 1.)
+function trace_prior(r,fittedparam,f=Normal,cv = 1.)
 	rcv = cv * ones(length(r))
 	distribution_array(log.(r[fittedparam]),sigmalognormal(rcv[fittedparam]),f)
 end
@@ -159,7 +159,7 @@ function read_tracefiles(path::String,cond::String,col=3)
             target = joinpath(root, file)
             if occursin(cond,target)
                 # println(target)
-                push!(traces, read_tracefile(path::String,col))
+                push!(traces, read_tracefile(target,col))
             end
         end
     end
