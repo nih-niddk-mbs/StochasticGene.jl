9467f86495b92aec2c08b94d1d8bf0193240c6d3
Carson C Chow <ccc1685@users.noreply.github.com>
2023-10-22 22:28:46 -0400

    updates

--- src/fit.jl
+++ src/fit.jl
@@ -67,7 +67,6 @@ Fit steady state or transient GM model to RNA data for a single gene, write the 
 - `optimize`: use optimizer to compute maximum likelihood value
 - `writesamples`: write out MH samples if true, default is false
 
-
 """
 function fit(nchains::Int, datatype::String, dttype::Vector, datapath, gene::String, cell::String, datacond::String, interval, nascent, infolder::String, resultfolder::String, inlabel::String, label::String,
     fittedparam::Vector, fixedeffects::Tuple, transitions::Tuple, G::Int, R::Int, S::Int, insertstep::Int, root=".", maxtime::Float64=60.0, priormean=Float64[], priorcv=10.0, nalleles=2, onstates=Int[], decayrate=-1.0, splicetype="", probfn=prob_GaussianMixture, noiseparams=5, weightind=5, ratetype="median",
@@ -82,8 +81,8 @@ function fit(nchains::Int, datatype::String, dttype::Vector, datapath, gene::Str
     ~occursin("trace", lowercase(datatype)) && (noiseparams = 0)
     decayrate < 0 && (decayrate = get_decay(gene, cell, root))
     isempty(priormean) && (priormean = prior_ratemean(transitions, R, S, insertstep, decayrate, noiseparams, weightind))
-    isempty(fittedparam) && (fittedparam = collect(1:num_rates(transitions, R, S, insertstep)-1))
-
+    # isempty(fittedparam) && (fittedparam = collect(1:num_rates(transitions, R, S, insertstep)-1))
+    isempty(fittedparam) && (fittedparam = default_fittedparams(datatype, transitions, R, S, insertstep, noiseparams))
     r = readrates(infolder, inlabel, gene, G, R, S, insertstep, nalleles, ratetype)
     isempty(r) && (r = priormean)
     println(r)
@@ -92,14 +91,7 @@ function fit(nchains::Int, datatype::String, dttype::Vector, datapath, gene::Str
     fit(nchains, data, model, options, resultfolder, burst, optimize, writesamples)
 end
 
-function default_fittedparams(datatype, transitions, R, S, insertstep, noiseparams)
-    n = num_rates(transitions, R, S, insertstep)
-    fittedparam = collect(1:n-1)
-    if occursin("trace", datatype)
-        fittedparam = vcat(fittedparam, collect(n:n+noiseparams))
-    end
-    fittedparam
-end
+
 """
     fit(; nchains::Int=2, datatype::String="", dttype=String[], datapath="", cell::String="", datacond="", interval=1.0, nascent=0.5, infolder::String="", resultfolder::String="test", inlabel::String="", label::String="",
     fittedparam::Vector=Int[], fixedeffects::Tuple=tuple(), transitions::Tuple=([1, 2], [2, 1]), G::Int=2, R::Int=0, S::Int=0, insertstep::Int=1, root=".", priormean=Float64[], nalleles=2, priorcv=10.0, onstates=Int[], decayrate=-1.0, splicetype="", probfn=prob_GaussianMixture, noiseparams=5, weightind=5, ratetype="median",
@@ -107,7 +99,7 @@ end
 
 
 """
-function fit(; nchains::Int=2, datatype::String="rna", dttype=String[], datapath="HCT116_testdata/", cell::String="HCT116", datacond="MOCK", interval=1.0, nascent=0.5, infolder::String="HCT116_test", resultfolder::String="HCT116_test", inlabel::String="", label::String="",
+function fit(; nchains::Int=2, datatype::String="rna", dttype=String[], datapath="HCT116_testdata/", gene = "MYC", cell::String="HCT116", datacond="MOCK", interval=1.0, nascent=0.5, infolder::String="HCT116_test", resultfolder::String="HCT116_test", inlabel::String="", label::String="",
     fittedparam::Vector=Int[], fixedeffects::Tuple=tuple(), transitions::Tuple=([1, 2], [2, 1]), G::Int=2, R::Int=0, S::Int=0, insertstep::Int=1, root=".", priormean=Float64[], nalleles=2, priorcv=10.0, onstates=Int[], decayrate=-1.0, splicetype="", probfn=prob_GaussianMixture, noiseparams=5, weightind=5, ratetype="median",
     propcv=0.01, maxtime::Float64=60.0, samplesteps::Int=1000000, warmupsteps=0, annealsteps=0, temp=1.0, tempanneal=100.0, temprna=1.0, burst=false, optimize=false, writesamples=false, src="")
     if R > 0
@@ -123,7 +115,6 @@ function fit(; nchains::Int=2, datatype::String="rna", dttype=String[], datapath
         typeof(datacond) <: AbstractString && (label = label * "_" * datacond)
     end
     isempty(inlabel) && (inlabel = label)
-
     fit(nchains, datatype, dttype, datapath, gene, cell, datacond, interval, nascent, infolder, resultfolder, inlabel, label, fittedparam, fixedeffects, transitions, G, R, S, insertstep, root, maxtime, priormean, priorcv, nalleles, onstates, decayrate, splicetype, probfn, noiseparams, weightind, ratetype,
         propcv, samplesteps, warmupsteps, annealsteps, temp, tempanneal, temprna, burst, optimize, writesamples)
 
@@ -153,8 +144,8 @@ function fit(nchains, data, model, options, resultfolder, burst, optimize, write
     end
     finalize(data, model, fits, stats, measures, options.temp, resultfolder, optimized, bs, writesamples)
     println(now())
-    get_rates(stats.medparam, model, false)
-    return fits, stats, measures
+    # get_rates(stats.medparam, model, false)
+    return fits, stats, measures, data, model, options
 end
 
 
@@ -297,7 +288,19 @@ function prior_ratemean(transitions::Tuple, R::Int, S::Int, insertstep, decayrat
     end
     rm
 end
+"""
+    default_fittedparams(datatype, transitions, R, S, insertstep, noiseparams)
 
+create vector of fittedparams that includes all rates except the decay time
+"""
+function default_fittedparams(datatype, transitions, R, S, insertstep, noiseparams)
+    n = num_rates(transitions, R, S, insertstep)
+    fittedparam = collect(1:n-1)
+    if occursin("trace", datatype)
+        fittedparam = vcat(fittedparam, collect(n+1:n+noiseparams))
+    end
+    fittedparam
+end
 
 """
 lossfn(x,data,model)
