719f305ef8e782c4af669de1576d527d228e613c
Carson C Chow <ccc1685@users.noreply.github.com>
2025-01-21 09:14:21 -0500

    add composable branch

--- src/common.jl
+++ src/common.jl
@@ -201,7 +201,69 @@ struct HMMReporter
     offstates::Vector{Int}
 end
 
+#=
+Model Traits
+=#
 
+"""
+    Abstract type for model traits that define different capabilities.
+"""
+abstract type ModelTrait end
+
+"""
+    Base GRSM trait that all models must have.
+"""
+struct GRSMTrait <: ModelTrait end
+
+"""
+    CouplingTrait
+
+Trait for models that have coupling between units.
+
+# Fields
+- `source_models::Tuple`: Model indices for each unit (e.g., (1,1,2) means units 1 and 2 use model 1, unit 3 uses model 2)
+- `source_units::Tuple`: Source units for each unit (e.g., ([2,3], [1], Int[]) means unit 1 influenced by 2,3; unit 2 by 1; unit 3 by none)
+- `source_states::Tuple`: Source states (e.g., (3,0) means model 1 influences in G state 3, model 2 doesn't influence)
+- `target_transitions::Tuple`: Target transitions (e.g., (0,4) means model 1 not influenced, model 2 influenced at G transition 4)
+- `n_coupling_params::Int`: Number of coupling parameters
+"""
+struct CouplingTrait <: ModelTrait
+    source_models::Tuple
+    source_units::Tuple
+    source_states::Tuple
+    target_transitions::Tuple
+    n_coupling_params::Int
+end
+
+"""
+    HierarchicalTrait
+
+Trait for hierarchical models that share parameters across different levels.
+
+# Fields
+- `hierarchy::Hierarchy`: Hierarchy of models for hierarchical modeling
+"""
+struct HierarchicalTrait <: ModelTrait
+    hierarchy::Hierarchy
+end
+
+"""
+    GridTrait
+
+Trait for models that use grid-based parameter space exploration.
+
+# Fields
+- `rate_range::UnitRange`: Range of rate parameters
+- `noise_range::UnitRange`: Range of noise parameters
+- `grid_range::UnitRange`: Range of grid points
+- `n_grid::Int`: Number of grid points
+"""
+struct GridTrait <: ModelTrait
+    rate_range::UnitRange
+    noise_range::UnitRange
+    grid_range::UnitRange
+    n_grid::Int
+end
 
 """
     Abstract model types
@@ -290,6 +352,25 @@ Structure for GRSM models.
 - `reporter::ReporterType`: Vector of reporters or sojourn states (onstates) or vectors of vectors depending on model and data.
 """
 struct GRSMmodel{RateType,PriorType,ProposalType,ParamType,MethodType,ComponentType,ReporterType} <: AbstractGRSMmodel{RateType,ReporterType}
+    rates::RateType
+    Gtransitions::Tuple
+    G::Int
+    R::Int
+    S::Int
+    insertstep::Int
+    nalleles::Int
+    splicetype::String
+    rateprior::PriorType
+    proposal::ProposalType
+    fittedparam::ParamType
+    fixedeffects::Tuple
+    method::MethodType
+    components::ComponentType
+    reporter::ReporterType
+end
+
+struct GRSMtraitmodel{} <: AbstractGRSMmodel{RateType,ReporterType}
+    traits::NamedTuple
     rates::RateType
     Gtransitions::Tuple
     G::Int
